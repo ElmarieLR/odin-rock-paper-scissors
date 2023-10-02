7a4754be3e4c072f92f5efa539548f6f0e67c10c
Elmarie le Roux <140609114+ElmarieLR@users.noreply.github.com>
2023-10-02 23:02:22 +0200

    Add player input, computer choice and outcome

--- myscripts.js
+++ myscripts.js
@@ -1,27 +1,50 @@
-//Rock, Paper, Scissors, Lozard, Spock
+//played from console
 
+//Computer choice - randomly return 'Rock’, ‘Paper’ or ‘Scissors’
 
-console.log("Hello World")
+const choiceArray = ["Rock", "Paper", "Scissors"];
 
-// This game is going to be played completely from the console, so don’t worry about putting anything else in there.
-//Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
-//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-//Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-//Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
-/*function playRound(playerSelection, computerSelection) {
-  // your code here!
+function getComputerChoice() {
+  return choiceArray[(Math.floor(Math.random() * choiceArray.length))];
 }
- 
-const playerSelection = "rock";
+
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));*/
+console.log(computerSelection);
+
+//Prompt player choice with case-insensitive parameter
+
+let playerInput = prompt("What is your choice: Rock, Paper of Scissors", "rock");
+
+function caseInsensitive(playerInput) {
+  playerInput = playerInput.toLowerCase();
+  return(playerInput[0].toUpperCase() +
+      playerInput.slice(1));
+}
+
+const playerSelection = caseInsensitive(playerInput); 
+
+//Play single round of RPS and return a string that declares the winner of the round 
+
+function playRound(playerSelection, computerSelection) {
+  let roundOutcome = ""
+  if (playerSelection === computerSelection) {
+    return roundOutcome = "It's a tie";
+  } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+    return roundOutcome = `${playerSelection} beats ${computerSelection}. You win this round!`;
+  } else {
+    return roundOutcome = `${computerSelection} beats ${playerSelection}. You lose this round!`;
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
 
 //Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 //You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
 //At this point you should be using console.log() to display the results of each round and the winner at the end.
-//Use prompt() to get input from the user. Read the docs here if you need to.
+
 //Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
 //Feel free to create more “helper” functions if you think it would be useful.
+
+
 //When making interactive projects, like this one, you might be tempted to add more features, improve interactivity, user experience, design and styling of your website, and so on.
 //We recommend not doing that, and saving this effort for your portfolio projects.
