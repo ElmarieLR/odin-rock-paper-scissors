deb59b7d00acd7cf6af1af7069ccfaa0060929d4
Elmarie le Roux <140609114+ElmarieLR@users.noreply.github.com>
2023-10-03 21:20:30 +0200

    Add rounds and score to the game

--- myscripts.js
+++ myscripts.js
@@ -1,50 +1,69 @@
-//played from console
+//played from console - keep simple. Design will be added later.
 
-//Computer choice - randomly return 'Rock’, ‘Paper’ or ‘Scissors’
+let score = 0;
 
 const choiceArray = ["Rock", "Paper", "Scissors"];
 
-function getComputerChoice() {
-  return choiceArray[(Math.floor(Math.random() * choiceArray.length))];
-}
+//This is a 5 round game that keeps score and reports a winner or loser at the end.
+//The computer's selection and the results of each round are also displayed.
 
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
+function game() {
 
-//Prompt player choice with case-insensitive parameter
+  for (let rounds = 1; rounds <= 5; rounds++) {
 
-let playerInput = prompt("What is your choice: Rock, Paper of Scissors", "rock");
+    //Prompt player choice with case-insensitive parameter
 
-function caseInsensitive(playerInput) {
-  playerInput = playerInput.toLowerCase();
-  return(playerInput[0].toUpperCase() +
-      playerInput.slice(1));
-}
+    let playerInput = prompt("What is your choice: Rock, Paper or Scissors", "rock");
 
-const playerSelection = caseInsensitive(playerInput); 
+    function caseInsensitive(userInput) {
+      userInput = userInput.toLowerCase();
+      return(userInput[0].toUpperCase() + userInput.slice(1));
+    }
 
-//Play single round of RPS and return a string that declares the winner of the round 
+    const playerSelection = caseInsensitive(playerInput);
 
-function playRound(playerSelection, computerSelection) {
-  let roundOutcome = ""
-  if (playerSelection === computerSelection) {
-    return roundOutcome = "It's a tie";
-  } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
-    return roundOutcome = `${playerSelection} beats ${computerSelection}. You win this round!`;
-  } else {
-    return roundOutcome = `${computerSelection} beats ${playerSelection}. You lose this round!`;
+    //Computer choice - randomly return 'Rock’, ‘Paper’ or ‘Scissors’
+
+    function getComputerChoice() {
+      return choiceArray[(Math.floor(Math.random() * choiceArray.length))];
+    }
+    
+    const computerSelection = getComputerChoice();
+    
+    console.log(`The computer chose ${computerSelection}`);
+
+    //Play single round of RPS and return a string that declares the winner of the round 
+
+    function playRound(playerSelection, computerSelection) {
+      let roundOutcome = ""
+      if (playerSelection === computerSelection) {
+        rounds--;
+        return roundOutcome = `You both chose ${playerSelection}. It's a tie.`;
+      } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        score++;
+        return roundOutcome = `${playerSelection} beats ${computerSelection}. You win this round! Score: ${score}/${rounds}`;
+      } else {
+        return roundOutcome = `${computerSelection} beats ${playerSelection}. You lose this round! Score: ${score}/${rounds}`;
+      }
+    }
+
+    console.log(playRound(playerSelection, computerSelection));
   }
+  
+  //Who won the game
+  let gameOutcome = "";
+  if (score >= 3) {
+    return gameOutcome = `You won ${score} out of 5 rounds. You win the game!`
+  } else {
+    return gameOutcome = `You won ${score} out of 5 rounds. The computer wins the game!`
+  }
+  
+  console.log(gameOutcome);
 }
 
-console.log(playRound(playerSelection, computerSelection));
-
-//Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-//You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
-//At this point you should be using console.log() to display the results of each round and the winner at the end.
-
-//Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-//Feel free to create more “helper” functions if you think it would be useful.
+console.log(game());
 
 
-//When making interactive projects, like this one, you might be tempted to add more features, improve interactivity, user experience, design and styling of your website, and so on.
-//We recommend not doing that, and saving this effort for your portfolio projects.
+
+
+
