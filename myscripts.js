79d5fb7909de5f7f0e75774c630f29c10cd4abe3
Elmarie le Roux <140609114+ElmarieLR@users.noreply.github.com>
2023-11-21 22:01:55 +0200

    Add player and computer decision

--- myscripts.js
+++ myscripts.js
@@ -1,82 +1,55 @@
 let playerScore = 0;
 let computerScore = 0;
 
+//Computer choice - randomly return 'Rock’, ‘Paper’ or ‘Scissors’
+
 const choiceArray = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+  return choiceArray[(Math.floor(Math.random() * choiceArray.length))];
+}
 
 //This game keeps score until a player reaches 5 points and then reports the winner.
 //The computer's selection and the results of each round are displayed.
 
+let startButton = document.querySelector("#startBtn");
+startButton.addEventListener("click", game);
+
 function game() {
+  document.querySelector("#start").style.display = "none";
+  document.querySelector("#play").style.display = "block";
+  console.log("started");
 
-  while (playerScore < 5 && computerScore< 5) {
 
-    
-    let playerSelection = "";
-    let computerSelection = "";
-
-    //Player chooses with button
+  // Player chooses with button, event listeners takes player choice a
     
     let rockButton = document.querySelector("#rock");
-    rockButton.addEventListener('click', playRound);
+    rockButton.addEventListener("click", choices);
 
     let paperButton = document.querySelector("#paper");
-    paperButton.addEventListener('click', playRound);
+    paperButton.addEventListener("click", choices);
 
     let scissorsButton = document.querySelector("#scissors");
-    scissorsButton.addEventListener('click', playRound);
-
-    //Play single round of RPS and declares the winner of the round
-
-    function playRound(playerSelection, computerSelection) {
-      //Player choice
-      let playerSelection = document.getElementsByClassName("choice").getAttribute("value");
-
-      //Computer choice - randomly return 'Rock’, ‘Paper’ or ‘Scissors’
-      computerSelection =  () => choiceArray[(Math.floor(Math.random() * choiceArray.length))];
-
-      //Display player and computer choices
-      const playerDecision = document.querySelector("#player_selection");
-      playerDecision.textContent = `Player chose ${playerSelection}.`;
-
-      const computerDecision = document.querySelector("#computer_selection");
-      computerDecision.textContent = `Computer chose ${computerSelection()}.`;
-
-      // Play round
-      let roundOutcome = "";
-      if (playerSelection === computerSelection) {
-        return roundOutcome = 
-          `You both chose ${playerSelection}. It's a tie.`;
-      } else if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") || 
-        (playerSelection === "Paper" && computerSelection === "Rock") || 
-        (playerSelection === "Scissors" && computerSelection === "Paper")
-        ) {
-          humanScore++;
-        return roundOutcome = 
-          `${playerSelection} beats ${computerSelection}. 
-          You win this round!`;
-      } else {
-        computerScore++;
-        return roundOutcome = 
-          `${computerSelection} beats ${playerSelection}. 
-          You lose this round!`;
-      }
+    scissorsButton.addEventListener("click", choices);
+  
+  // Define choices and start a round
+    function choices(event) {
+      const playerSelection = event.target.value;
+      const computerSelection = getComputerChoice();
+      console.log(playerSelection);
+      console.log(computerSelection);
+      playRound(playerSelection, computerSelection);
     }
 
-    console.log(playRound(playerSelection, computerSelection));
-  }
+    function playRound(playerSelection, computerSelection) {
+      console.log("play");
+      
+      // Display player and computer choices
+      const playerDecision = document.querySelector("#player_selection");
+      playerDecision.textContent = `Player chose ${playerSelection}.`;
+      
+      const computerDecision = document.querySelector("#computer_selection");
+      computerDecision.textContent = `Computer chose ${computerSelection}.`;
+    }
   
-  //Who won the game
-  let gameOutcome = "";
-  if (humanScore === 5) {
-    return gameOutcome = 
-      `You win the game!`;
-  } else {
-    return gameOutcome = 
-      `The computer wins the game!`;
-  }
-  
-  console.log(gameOutcome);
 }
-
-console.log(game());
