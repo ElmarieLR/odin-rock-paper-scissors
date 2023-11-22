546148219be190d9ed802646df62ee467ca9b417
Elmarie le Roux <140609114+ElmarieLR@users.noreply.github.com>
2023-11-22 22:06:47 +0200

    Add start again function

--- myscripts.js
+++ myscripts.js
@@ -10,80 +10,106 @@ function getComputerChoice() {
   return choiceArray[(Math.floor(Math.random() * choiceArray.length))];
 }
 
-//This game keeps score until a player reaches 5 points and then reports the winner.
-//The computer's selection and the results of each round are displayed.
+// Start
 
-let startButton = document.querySelector("#startBtn");
-startButton.addEventListener("click", game);
+document.querySelector("#startBtn").addEventListener("click", game);
+
+//This game keeps score until a player reaches 5 points and then reports the winner.
+//The computer's selection and the results of each round are displayed. 
 
 function game() {
   document.querySelector("#start").style.display = "none";
   document.querySelector("#play").style.display = "block";
+  document.querySelector("#results").style.display = "block";
   console.log("started");
 
-
-    // Player chooses with button, event listeners takes player choice a
+  // Player chooses with button, event listeners takes player choice 
     
-    let rockButton = document.querySelector("#rock");
-    rockButton.addEventListener("click", choices);
+  document.querySelector("#rock").addEventListener("click", choices);
 
-    let paperButton = document.querySelector("#paper");
-    paperButton.addEventListener("click", choices);
+  document.querySelector("#paper").addEventListener("click", choices);
 
-    let scissorsButton = document.querySelector("#scissors");
-    scissorsButton.addEventListener("click", choices);
+  document.querySelector("#scissors").addEventListener("click", choices);
   
-    // Define choices and start a round
+  // Define choices and start a round
 
-    function choices(event) {
-      const playerSelection = event.target.value;
-      const computerSelection = getComputerChoice();
+  function choices(event) {
+    const playerSelection = event.target.value;
+    const computerSelection = getComputerChoice();
+    // Display player and computer choices
 
-      // Display player and computer choices
+    const playerDecision = document.querySelector("#player_selection");
+    playerDecision.textContent = `Player chose ${playerSelection}.`;
 
-      const playerDecision = document.querySelector("#player_selection");
-      playerDecision.textContent = `Player chose ${playerSelection}.`;
+    const computerDecision = document.querySelector("#computer_selection");
+    computerDecision.textContent = `Computer chose ${computerSelection}.`;
 
-      const computerDecision = document.querySelector("#computer_selection");
-      computerDecision.textContent = `Computer chose ${computerSelection}.`;
+    playRound(playerSelection, computerSelection);
+  }
 
-      playRound(playerSelection, computerSelection);
-    }
+  // Winner of round 
 
-    //Play single round of RPS and declares the winner of the round
+  function playRound(playerSelection, computerSelection) {
+    console.log("play");
 
-    function playRound(playerSelection, computerSelection) {
-      console.log("play");
+    if (playerSelection === computerSelection) {
+      roundOutcome = `You both chose ${playerSelection}. It's a tie.
+      Player: ${playerScore}
+      Computer: ${computerScore}`;
+      console.log("tied");
+      roundResult();
+    } else if (
+      (playerSelection === "Rock" && computerSelection === "Scissors") || 
+      (playerSelection === "Paper" && computerSelection === "Rock") || 
+      (playerSelection === "Scissors" && computerSelection === "Paper")
+      ) {
+      playerScore++;
+      roundOutcome = 
+        `${playerSelection} beats ${computerSelection}. 
+        You win this round!
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
+      console.log("win");
+      roundResult();
+    } else {
+      computerScore++;
+      roundOutcome = 
+        `${computerSelection} beats ${playerSelection}. 
+        You lose this round!
+        Player: ${playerScore}
+        Computer: ${computerScore}`;
+      console.log("lose");
+      roundResult();
+    }      
+  }
+}
 
-      if (playerSelection === computerSelection) {
-        roundOutcome = 
-          `You both chose ${playerSelection}. It's a tie.`;
-        console.log("tied");
-        roundResult();
-      } else if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") || 
-        (playerSelection === "Paper" && computerSelection === "Rock") || 
-        (playerSelection === "Scissors" && computerSelection === "Paper")
-        ) {
-        playerScore++;
-        roundOutcome = 
-          `${playerSelection} beats ${computerSelection}. 
-          You win this round!`;
-        console.log("win");
-        roundResult();
-      } else {
-        computerScore++;
-        roundOutcome = 
-          `${computerSelection} beats ${playerSelection}. 
-          You lose this round!`;
-        console.log("lose");
-        roundResult();
-      } 
-    }
+function roundResult() {
+  const roundResultMessage = document.querySelector("#round-result");
+  roundResultMessage.textContent = roundOutcome;
+  if (playerScore < 5 && computerScore < 5) {
+    game();
+  } else {
+    document.querySelector("#play").style.display = "none";
+    endResult();
+  }
+}
 
-    function roundResult() {
-      const roundResultMessage = document.querySelector("#round-result");
-      roundResultMessage.textContent = roundOutcome;
-    }
-  
+function endResult() {
+  if (playerScore === 5) {
+    const roundResultMessage = document.querySelector("#score");
+    roundResultMessage.textContent = "CONGRATULATIONS! YOU WON!"
+  } else if (computerScore === 5) {
+    const roundResultMessage = document.querySelector("#score");
+    roundResultMessage.textContent = "Sorry! You lost!"
+  }
+  document.querySelector("#start_over").style.display = "block";
+}
+
+// Play again
+
+document.querySelector("#start_over_button").addEventListener("click", startAgain);
+
+function startAgain() {
+  location.reload();
 }
